dd8c6f4bf1127cca629ca8a4cd886a1946c73af3
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2019-02-02 19:13:06 +0000

    Ui Added
    Most of the main menu has been created. A basic battle UI also has been created. The player stats screen has also been created just needs actual stats to be fed into it.

---
+++ LuckTigerIsland/Assets/Scripts/UI/FOR TESTING ONLY/MenuOpen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class MenuOpen : MonoBehaviour {
+    public GameObject inventoryMenu;
+    public GameObject arrow;
+    EventSystem m_eventSystem;
+	// Use this for initialization
+	void Start () {
+        inventoryMenu.SetActive(false);
+        m_eventSystem = EventSystem.current;
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape))
+        {
+            inventoryMenu.SetActive(true);
+            m_eventSystem.SetSelectedGameObject(arrow);
+        }
+	}
+}
