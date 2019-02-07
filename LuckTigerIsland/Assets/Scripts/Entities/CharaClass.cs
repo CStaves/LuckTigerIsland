d5367dfe0a71e7241cb36a2becb8a6d9eea218cf
Chris Staves <c.staves@outlook.com>
2019-02-07 16:41:13 +0000

    Week 1: Entity
    Added:
    CharaClass.cs (Script)
    EnemChara.cs (Script)
    PlayerChara.cs (Script)
    TigerEnem.png (Art)
    EntityTest.unity (Scene)

---
+++ LuckTigerIsland/Assets/Scripts/Entities/CharaClass.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaClass : MonoBehaviour {
+
+    public int health;
+    public int strength; //basic attack
+    public int defense;
+    public int speed;
+    public int level;
+
+    public int tempDMGReduct = 0; //the amount of damage reduced to the intial damage
+    public int totalDMG = 0; //total amount of damage that goes through.
+    public int chanceToHit;
+
+    //CALCULATION IDEA: (atk: strength / def: defense = tempDMGReduct) atk: strength - tempDMGReduct = totalDMG
+    //e.g. (100 / 10 = 10) 100 - 10 = 90
+    //e.g. (56 / 8 = 7) 56 - 7 = 49
+
+    public bool attacking = false;
+    public bool dmgRecieve = false;
+    public bool dmgDealt = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        yield return new WaitForSeconds(1.5f);
+        health --;
+	}
+    //-----------------------------------------------------------------------------------------------------
+    //Setters and Getters
+    public void Sethealth(int m_health)
+    {
+        health = m_health;
+    }
+    public int GetHealth()
+    {
+        return health;
+    }
+    public int GetStrength()
+    {
+        return strength;
+    }
+
+    public int GetDefense()
+    {
+        return defense;
+    }
+
+    public int GetSpeed()
+    {
+        return speed;
+    }
+
+    public void SetLevel(int m_level)
+    {
+        level = m_level;
+    }
+    
+    public int GetLevel()
+    {
+        return level;
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+    protected void Attack()
+    {
+        if(attacking == true)
+        {
+            chanceToHit = Random.Range(1, 100);
+
+            if (chanceToHit <= 85)
+            {
+                dmgRecieve = true;
+            }
+            else
+                return;
+     
+        }
+    }
+
+    protected void Damage()
+    {
+        if (dmgRecieve == true)
+        {
+            GetStrength();
+            GetDefense();
+            //(GetStrength() / GetDefense() = tempDMGReduct) GetStrength - tempDMGReduct = totalDMG;
+            dmgRecieve = false;
+            dmgDealt = true;
+
+            if(dmgDealt == true)
+            {
+                //m_health - totalDMG;
+                dmgDealt = false;
+            }
+        }
+    }
+
+    protected void Death()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+     
+    }
+}
